bacc10ea3179b41396773195d2f3cde4b4ba6874
HabacucMX <habacuc.vera@gmail.com>
2022-10-08 23:03:28 -0500

    Quest base components

---
+++ packages/react-app/src/views/Biker.jsx
@@ -0,0 +1,5 @@
+export default function Biker() {
+    return (
+        <div>Cicla!</div>
+    )
+}
---
+++ packages/react-app/src/views/Scholar.jsx
@@ -0,0 +1,5 @@
+export default function scholar() {
+    return (
+        <div>Maestro!</div>
+    )
+}
